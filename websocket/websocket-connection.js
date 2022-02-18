4a7374d15f1d792f9956985b286c17e06b7d1738
tmaa <thomasaanum@gmail.com>
2022-02-18 10:35:05 -0500

    added heartbeat

--- websocket/websocket-connection.js
+++ websocket/websocket-connection.js
@@ -2,25 +2,32 @@ const queryString = require('query-string')
 const Message = require('../mongo_models/Message');
 const WebSocket = require('ws')
 
-var sockets = {}
+var clients = {}
+
+function heartbeat() {
+  this.isAlive = true;
+}
 
 const websocketConnection = (wss) => {
   wss.on("connection", async (ws, req, Client) => {
     const [path, params] = req.url.split('?')
     const urlParams = queryString.parse(params);
     console.log(`Client connected: ${urlParams.id}`);
-    sockets[urlParams.id] = ws
+    clients[urlParams.id] = ws
+    ws.isAlive = true
+
+    ws.on('pong', heartbeat);
 
     ws.on("message", async (data) => {
       const messageData = JSON.parse(data)
       console.log(messageData) 
-      const sendTo = sockets[messageData.to];
-      const message = new Message({
-        from: messageData.from,
-        to: messageData.to,
-        message: messageData.message
-      });
-      const newMessage = await message.save();
+      const sendTo = clients[messageData.to];
+      // const message = new Message({
+      //   from: messageData.from,
+      //   to: messageData.to,
+      //   message: messageData.message
+      // });
+      // const newMessage = await message.save();
       
       if (sendTo && sendTo.readyState === WebSocket.OPEN) {
         sendTo.send(JSON.stringify(messageData));
@@ -30,10 +37,22 @@ const websocketConnection = (wss) => {
     
     ws.on("close", async () => {
       console.log(`Client disconnected ${urlParams.id}`)
-      if(sockets[urlParams.id]){
-        delete sockets[urlParams.id]
+      if(clients[urlParams.id]){
+        delete clients[urlParams.id]
       }
-    })
+    });
+  });
+
+  const interval = setInterval(() => {
+    for(const key in clients){
+      clients[key].isAlive = false;
+      clients[key].ping();
+      console.log(`Client ${key}`)
+    }
+  }, 3000);
+
+  wss.on('close', function close() {
+    clearInterval(interval);
   });
 }
 
